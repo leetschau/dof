cc03b834b528c69b6a81044344e42abd2c4ca2f2
Li Chao <clouds@isrc.iscas.ac.cn>
2021-07-18 10:42:46 +0800

    output format more FP

--- Notes.fs
+++ Notes.fs
@@ -69,14 +69,12 @@ let saveAndDisplayList (notes: Note list) : string =
                         + "] "
                         + (note.TagList |> String.concat "; "))
                     notes
-    let noteCnt = List.length body
-    match noteCnt with
-    | 0 -> ""
-    | _ ->
-        let header = "No. Updated, Notebook, Title, Created, Tags"
-        let withIdx = List.zip [1 .. List.length body] body |>
-                      List.map (fun (idx, body) -> string idx + ". " + body)
-        (header :: withIdx) |> String.concat "\n"
+    let withIdx = List.zip [1 .. List.length body] body |>
+                  List.map (fun (idx, body) -> string idx + ". " + body)
+    let header = if List.length body > 0 then
+                    "No. Updated, Notebook, Title, Created, Tags"
+                 else ""
+    (header :: withIdx) |> String.concat "\n"
 
 
 let simpleSearch (args: string list) : string =
